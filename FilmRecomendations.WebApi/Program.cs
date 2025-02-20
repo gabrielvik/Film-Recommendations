f2ec2aadf74516b81ecf59a8a922715995ce4fd1
leobrowaldh <leobrowaldh@gmail.com>
2025-02-20 12:24:02 +0100

    configured httpclient in program

--- FilmRecomendations.WebApi/Program.cs
+++ FilmRecomendations.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,19 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient(name: "TMDb.WebApi",
+  configureClient: options =>
+  {
+      options.BaseAddress = builder.Configuration.GetSection("TMDb:BaseAddress").Get<Uri>();
+      options.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue(
+        "application/json", 1.0));
+
+      // Add the Authorization header here
+      options.DefaultRequestHeaders.Authorization =
+          new AuthenticationHeaderValue("Bearer", builder.Configuration["TMDb:ApiKey"]);
+  });
 
 var app = builder.Build();
 
