864aa097eff92184bfe516f6ada210039cba3531
leobrowaldh <leobrowaldh@gmail.com>
2025-02-20 15:28:58 +0100

    database almost done

---
+++ FilmRecomendations.Db/DbModels/AplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Database.Seeder;
+
+namespace FilmRecomendations.Db.DbModels;
+
+public class AplicationUser: IdentityUser
+{
+    public string? ProfilePicture { get; set; } = ProfilePictureSeeder.RandomizeProfilePicture();
+}
---
+++ FilmRecomendations.Db/DbModels/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmRecomendations.Db.DbModels;
+
+public class Movie
+{
+    public int MovieId { get; set; }
+    public required string Title { get; set; }
+    public int? ReleaseYear { get; set; }
+    public int DirectorId { get; set; }
+    public List<Genere> Generes { get; set; } = new List<Genere>();
+    public Director Director { get; set; } = new Director();
+    public List<Actor> Actors { get; set; } = new List<Actor>();
+}
