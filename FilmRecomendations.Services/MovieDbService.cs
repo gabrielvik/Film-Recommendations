4e13ae1cf24f5768671c2b4a74b59ad4f06c846d
leobrowaldh <leobrowaldh@gmail.com>
2025-02-20 11:52:55 +0100

    new branch

--- FilmRecomendations.Services/MovieDbService.cs
+++ FilmRecomendations.Services/MovieDbService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FilmRecomendations.Models.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,15 @@ using System.Threading.Tasks;
 
 namespace FilmRecomendations.Services;
 
-class MovieDbService: IMovieDbService
+class MovieDbService : IMovieDbService
 {
+    public Task<List<Movie>> GetMovieDetails(List<int> movieIds)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<int>> GetMovieIdsAsync(List<string> movieNames)
+    {
+        throw new NotImplementedException();
+    }
 }
