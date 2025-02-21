1634ab13dd7e2106d6a8b5849cabe03a721364bf
leobrowaldh <leobrowaldh@gmail.com>
2025-02-21 10:51:22 +0100

    made the Imovieservice work on a singular item instead of a lsit

--- FilmRecomendations.Services/IMovieDbService.cs
+++ FilmRecomendations.Services/IMovieDbService.cs
@@ -5,16 +5,16 @@ namespace FilmRecomendations.Services;
 public interface IMovieDbService
 {
     /// <summary>
-    /// Fetches all movie ids by searching the movie database by name
+    /// Fetches the movie id by searching the movie database by name and release year
     /// </summary>
     /// <param name=""></param>
     /// <returns></returns>
-    public Task<List<int>> GetMovieIdsAsync(List<string> movieNames);
+    public Task<int> GetMovieIdsAsync(string movieName, int releaseYear);
 
     /// <summary>
     /// fetches movie details by movie id
     /// </summary>
     /// <param name="movieIds"></param>
     /// <returns></returns>
-    public Task<List<Movie>> GetMovieDetails(List<int> movieIds);
+    public Movie GetMovieDetails(int movieId);
 }
