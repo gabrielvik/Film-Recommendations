864f2412b9aae90b6a02dc3cfe14abf80f662e1e
leobrowaldh <leobrowaldh@gmail.com>
2025-03-03 11:36:38 +0100

    fixed the order in program.cs that was causing routing and authorization issues

--- FilmRecomendations.WebApi/Controllers/MoviesController.cs
+++ FilmRecomendations.WebApi/Controllers/MoviesController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using FilmRecomendations.Db.DbModels;
 using FilmRecomendations.Db.Repos;
 using Microsoft.AspNetCore.Authorization;
@@ -9,14 +10,27 @@ namespace FilmRecomendations.WebApi.Controllers;
 [Authorize]
 [Route("api/[controller]")]
 [ApiController]
-public class MoviesController(IMovieRepo _movieRepo, UserManager<ApplicationUser> _userManager) : ControllerBase
+public class MoviesController : ControllerBase
 {
-    [HttpGet()]
+    private readonly IMovieRepo _movieRepo;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public MoviesController(IMovieRepo movieRepo, UserManager<ApplicationUser> userManager)
+    {
+        _movieRepo = movieRepo;
+        _userManager = userManager;
+    }
+    [HttpGet("watchlist")]
     [ProducesResponseType(200, Type = typeof(IEnumerable<MovieDbM>))]
     [ProducesResponseType(400, Type = typeof(string))]
     public async Task<IActionResult> GetWatchList(string? filter = null, int pageNumber = 0, int pageSize = 10)
     {
-        var user = await _userManager.GetUserAsync(User);
+        var username = _userManager.GetUserName(User);
+        if (username == null)
+        {
+            return BadRequest("User not found");
+        }
+        var user = await _userManager.FindByNameAsync(username);
         if (user == null)
         {
             return BadRequest("User not found");
