77883b7924729ca78c01439e6bd4220481178637
Martin Olsson <martinolsson89@gmail.com>
2025-05-29 14:42:00 +0200

    styling fix 2

--- FilmRecommendations.Frontend/vite.config.js
+++ FilmRecommendations.Frontend/vite.config.js
@@ -2,11 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    // Ensure CSS is extracted to its own files
     cssCodeSplit: true,
-    // Improve SWA compatibility
     outDir: 'dist',
-    // Fix asset paths
     assetsDir: 'assets'
   }
 });
